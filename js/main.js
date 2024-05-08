763d541a971ba507805c615d47ca9213ef394d03
Jirashcha <120640294+Jirashcha@users.noreply.github.com>
2024-05-09 06:48:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ var base_url = window.location.origin;
 let model;
 (async function(){  
     console.log("model loading...");  
-    model = await tf.loadLayersModel("https://jirashcha.github.io/Japanese-Basic-Nature-Kanji-Prediction/models/model.json")
+    model = await tf.loadLayersModel("https://maneprajakta.github.io/Digit_Recognition_Web_App/models/model.json")
     console.log("model loaded..");
 })();
 
